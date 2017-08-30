7d81983a651802bff7a2cdfd01940d19f6abce10
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-08-30 22:45:20 +0700

    add some comment

--- week5/nBaseToDec.py
+++ week5/nBaseToDec.py
@@ -1,10 +1,10 @@
 base = int(input("From base : "))
 num = int(input("Input : "))
-ans = 0
-i=0
+ans = 0                 #Answer
+i = 0                   
 while(num>0):
-    tmp = num%10
-    ans = ans + tmp * base**i
-    num = num//10
-    i += 1
+    tmp = num%10            #Get the last digit of number
+    ans += tmp * base**i    #Add every value of digit to Answer
+    num = num//10           #Prepare num for next repeat
+    i += 1                
 print(ans)
