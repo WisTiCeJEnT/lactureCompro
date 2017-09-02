b19d6ba9b4e7478cc4dada6310893e553699855c
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-09-02 09:09:34 +0700

    return list of prime

--- prime.py
+++ prime.py
@@ -1,17 +1,16 @@
-n = int(input())    #input n>=2
-pls = []
-ls = []
-ans = 0
-for i in range(2,n+1):
-    ls.append(i)
-while(ls):
-    tmp = ls[0]
-    pls.append(ls.pop(0))
-    if(tmp*tmp<=n):
-        for i in ls:
-            if(i%tmp==0):
-                 ls.remove(i)
-    else:
-        pls.extend(ls)
-        break
-print(pls)
+def plist(n):
+    pls = []
+    ls = []
+    for i in range(2,n+1):
+        ls.append(i)
+    while(ls):
+        tmp = ls[0]
+        pls.append(ls.pop(0))
+        if(tmp*tmp<=n):
+            for i in ls:
+                if(i%tmp==0):
+                     ls.remove(i)
+        else:
+            pls.extend(ls)
+            break
+    return pls          #return list of prime
