2cd5b9a5358ad26eb6727c4b99edbf056dca4cee
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-09-14 01:04:53 +0700

    edit output :art:

--- week7-review/savingAccount.py
+++ week7-review/savingAccount.py
@@ -2,4 +2,4 @@ from math import pow
 p = float(input("Principal = "))
 r = float(input("Rate = "))
 n = int(input("Years = "))
-print(p*pow((100+r)/100,n))
+print("Amount = {:.5f}".format(p*pow((100+r)/100,n)))
