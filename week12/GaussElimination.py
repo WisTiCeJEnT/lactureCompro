bceb41d6bcfeee0d5ec5129f26951d817bbfaea5
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-10-27 14:32:00 +0700

    forwardE in con

--- week12/GaussElimination.py
+++ week12/GaussElimination.py
@@ -10,8 +10,17 @@ def inputlis():
         tmplis.append(list(map(int,input().split())))
     return tmplis
     
+def forwardE(mat):
+    l = len(mat)
+    for i in range(1,l):
+        tmp = mat[i-1][i-1]
+        tmp2 = mat[i-1][i]
+        for j in range(i,l):
+            mat[i][j] = mat[i][j] - mat[i-1][j]*tmp2/tmp
+    return mat        
+
 n = int(input())
 m = int(input())
 lis = inputlis()
-
+lis = forwardE(lis)
 printlis(lis)
