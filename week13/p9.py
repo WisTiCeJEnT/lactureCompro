f9f00f18ee21f403ee72d0b90a71858b5e10c5d6
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-11-01 10:23:23 +0700

    swap add never use div0

--- week13/p9.py
+++ week13/p9.py
@@ -3,11 +3,11 @@ while True:
         num = int(input('Input the numerator: ')) 
         den = int(input('Input the denomerator: ')) 
         print('%d//%d = %d'%(num, den, num//den))
-    except ZeroDivisionError:
-        print(' Devision or modulo by zero occurs!!') 
-        print(' Try next input..')
     except Exception as e:
         print('Exception occurs: ' + str(e)) 
         print('Try next input..')
+    except ZeroDivisionError:
+        print(' Devision or modulo by zero occurs!!') 
+        print(' Try next input..')
     else:
         print('Very good, next input..')
