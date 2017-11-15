4f00e8163cf86e02ce16822b31bfa791daffb9c2
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-11-15 16:41:14 +0700

    tuned

--- week11/GaussElimination.py
+++ week11/GaussElimination.py
@@ -11,9 +11,11 @@ def inputlis():
     return tmplis
     
 def forwardE(mat):
-    k = mat[0][0]/mat[1][0]
-    for i in range(m):
-        mat[1][i] = mat[1][i]*k - mat[0][i]
+    for j in range(1,n):
+        k = mat[j][0]/mat[0][0]
+        for i in range(m):
+            #mat[j][i] = mat[j][i]*k - mat[0][i]
+            mat[j][i] = mat[j][i] - mat[0][i]*k
     return mat        
 
 n = int(input())
