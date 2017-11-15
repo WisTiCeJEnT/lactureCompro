f0aeda88c78dddeb24b674b838207734495a1a93
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-11-15 20:54:26 +0700

    pivoting with merge sort

--- week11/GaussElimination.py
+++ week11/GaussElimination.py
@@ -16,6 +16,7 @@ def inputlis():
     
 def forwardE(mat):
     for k in range(n):
+        mat = mat[0:k] + pivoting(mat[k:len(mat)],k)
         for j in range(k+1,n):
             c = mat[j][k]/mat[k][k]
             for i in range(m):
@@ -39,9 +40,32 @@ def getX(mat):
 def printX(mat):
     for i in range(n):
         print("X{} = {:.5f}".format(i+1,mat[i][m-1]))
+
+def pivoting(mat,col):
+    if len(mat) == 1:
+        return mat
+    lisA = pivoting(mat[0:len(mat)//2],col)
+    lisB = pivoting(mat[len(mat)//2 : len(mat)],col)
+    #print("A:B",lisA,lisB)
+    lisS = []
+    while lisA or lisB:
+        if not lisA:
+            lisS.extend(lisB)
+            break
+        elif not lisB:
+            lisS.extend(lisA)
+            break
+        if lisA[0][col]<lisB[0][col]:
+            lisS.append(lisB.pop(0)) 
+        else:
+            lisS.append(lisA.pop(0)) 
+    #print(lisS)
+    return lisS
+    
 n = int(input())
 m = n+1
 lis = inputlis()
+#
 lis = forwardE(lis)
 lis = backwardE(lis)
 lis = getX(lis)
